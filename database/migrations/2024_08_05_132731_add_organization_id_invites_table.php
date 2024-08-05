6f4ac42adb457332b6a26124097805984b1d01eb
Andrew Sears <andrew@mayfifteenth.com>
2024-08-05 08:31:16 -0500

    Add migration and update build assets

---
+++ database/migrations/2024_08_05_132731_add_organization_id_invites_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invites', function (Blueprint $table) {
+            $table->string('organization_id')->after('user_id');
+            $table->foreign('organization_id')->references('id')->on('organizations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invites', function (Blueprint $table) {
+            $table->dropForeign(['organization_id']);
+        });
+    }
+};
